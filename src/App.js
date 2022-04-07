f2e115bcb52ceaf5a9d89bd091de5155f399b98c
Dj Hammond <59342048+jrvgon@users.noreply.github.com>
2022-04-07 15:20:23 -0500

    caught between react v17 and v18

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-// import logo from './logo.svg'
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Login from './components/Login'
 import './App.css'
 
 function App() {
-	return <h2>Hello! Let's built the disney+ clone.</h2>
+	return (
+		<div className='App'>
+			<Router>
+				<Link>
+					<Route exact path='/' />
+					<Login />
+					<Route />
+				</Link>
+			</Router>
+		</div>
+	)
 }
 
 export default App
